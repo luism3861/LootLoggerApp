1bd1536c662e53811bce701b3e3051656d57f6b3
luism3861 <eduardo_media@fragua.com.mx>
2020-11-09 20:55:53 -0600

    Initial Commit

---
+++ LootLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LootLogger
+//
+//  Created by Luis Eduardo Madina Huerta on 09/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
